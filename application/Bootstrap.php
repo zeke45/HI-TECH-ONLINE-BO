d62f927b9dfa4c24d139cc7e316c75b1ed4fb221
Vincent <codet.vincent@gmail.com>
2015-08-25 14:45:56 +0200

    test

---
+++ application/Bootstrap.php
@@ -0,0 +1,52 @@
+<?php
+
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
+{
+
+//    protected function _initResources()
+//    {
+//        if (!$this->hasResource('frontcontroller')) {
+//            $this->bootstrap('frontcontroller');
+//        }
+//
+//    }
+
+    protected function _initZFDebug()
+    {
+        $autoloader = Zend_Loader_Autoloader::getInstance();
+        $autoloader->registerNamespace('ZFDebug');
+
+        $options = array(
+            'plugins' => array('Variables',
+                'File' => array('base_path' => ROOT),
+                'Memory',
+                'Time',
+                'Registry',
+                'Exception'),
+            'jquery_path'       => '/js/jquery-2.1.3.min.js',
+        );
+
+        # Instantiate the database adapter and setup the plugin.
+        # Alternatively just add the plugin like above and rely on the autodiscovery feature.
+        if ($this->hasPluginResource('db')) {
+            $this->bootstrap('db');
+            $db = $this->getPluginResource('db')->getDbAdapter();
+            $options['plugins']['Database']['adapter'] = $db;
+        }
+
+        # Setup the cache plugin
+        if ($this->hasPluginResource('cache')) {
+            $this->bootstrap('cache');
+            $cache = $this-getPluginResource('cache')->getDbAdapter();
+            $options['plugins']['Cache']['backend'] = $cache->getBackend();
+        }
+
+        $debug = new ZFDebug_Controller_Plugin_Debug($options);
+
+        $this->bootstrap('frontController');
+        $frontController = $this->getResource('frontController');
+        $frontController->registerPlugin($debug);
+    }
+
+}
+
---
+++ application/modules/core/controllers/ErrorController.php
@@ -0,0 +1,18 @@
+<?php
+
+class ErrorController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function errorAction()
+    {
+        // action body
+    }
+
+
+}
+
