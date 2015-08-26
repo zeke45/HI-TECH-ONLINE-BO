36c672efe794e8fa85ee1c56fecf39704c409b28
Vincent <codet.vincent@gmail.com>
2015-08-26 11:02:15 +0200

    Ajouts Controller & pages index

---
+++ application/modules/core/controllers/CredentialController.php
@@ -0,0 +1,14 @@
+<?php
+
+class CredentialController extends Zend_Controller_Action
+{
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+}
